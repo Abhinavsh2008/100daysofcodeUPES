732c449c735df859c2242edf1b17babd594fac32
Abhinavsh2008 <abhisharma17072008@gmail.com>
2026-09-17 13:26:49 +0530

    Add files via upload

---
+++ day25code.c
@@ -0,0 +1,62 @@
+/*Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+*/
+
+#include<stdio.h>
+int main()
+    {
+        int i=5 , j ;
+       
+
+        for(i=5 ; i>=1 ; i--)
+        {
+            for(j=i ; j<=5 ; j++)
+            {
+                printf("%d",j);
+            }
+            printf("\n");
+        }
+        return 0;
+    }
+
+
+
+
+    /*Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+
+#include<stdio.h>
+    int main()
+        {
+            int a=5;//number of rows
+            int i;
+            int j;
+            int s;//space
+            
+            for(i=5 ; i>=1 ; i--)
+            {
+                for(s=0;s<=a-i;s++)
+                {
+                    printf(" ");
+                }
+                for(j=1;j<=i;j++)
+                {
+                    printf("*");
+                }
+                    
+                    printf("\n");
+            }
+            
+            return 0;
+
+        }
+    
