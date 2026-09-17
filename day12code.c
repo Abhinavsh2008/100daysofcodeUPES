cc49ea6dcc8a004f92b181afbb104687d76bfe02
Abhinavsh2008 <abhisharma17072008@gmail.com>
2026-09-17 13:22:29 +0530

    Add files via upload

---
+++ day12code.c
@@ -0,0 +1,16 @@
+//wap to calcultate library fine based on late days as follows?
+#include<stdio.h>
+int main(){
+    int days;
+    int fine=0;
+    printf("enter the numbersof days u r late:\n");
+    scanf("%d",&days);
+    if(days>=1 && days<=5){
+        printf("Your fine is :%d\n",fine=days*2);
+    }
+   
+
+
+
+
+    
