d5cc62bd3de204caf08aabaf3aff482c06af34cc
Abhinavsh2008 <abhisharma17072008@gmail.com>
2026-09-20 19:50:27 +0530

    Add files via upload

---
+++ day41code.c
@@ -0,0 +1,62 @@
+//Q81: Count characters in a string without using built-in length functions.
+
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+ 
+Output 2:
+1
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while (str[count] != '\0' && str[count] != '\n') {
+        count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
+
+
+
+//Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
